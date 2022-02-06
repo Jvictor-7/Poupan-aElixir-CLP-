69c152e7f4ea1f2e297ad697c4057bc600819581
Jvictor-7 <jvcl@ic.ufal.br>
2022-02-06 15:32:47 -0300

    first commit

---
+++ lib/oxebank.ex
@@ -0,0 +1,18 @@
+defmodule Oxebank do
+  @moduledoc """
+  Documentation for `Oxebank`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oxebank.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oxebank_test.exs
@@ -0,0 +1,8 @@
+defmodule OxebankTest do
+  use ExUnit.Case
+  doctest Oxebank
+
+  test "greets the world" do
+    assert Oxebank.hello() == :world
+  end
+end
