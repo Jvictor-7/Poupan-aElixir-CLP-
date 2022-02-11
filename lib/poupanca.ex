068ff4626a907c1b153d3001e75a4873edecf98a
Jvictor-7 <jvcl@ic.ufal.br>
2022-02-11 16:14:28 -0300

    Sprint6 - Final

--- lib/poupanca.ex
+++ lib/poupanca.ex
@@ -1,80 +1,127 @@
 defmodule Poupanca do
-  #Definimos que a criação de conta começa com 300 de saldo, para testarmos as funções
+  # Definimos que a criação de conta começa com 300 de saldo, para testarmos as funções
   defstruct usuario: Usuario, saldo: 300
-  #Definindo o "contas.txt" em @contas
+  # Definindo o "contas.txt" em @contas
   @contas "contas.txt"
 
-  #Função de cadastrar uma nova conta poupança, além disso transforma a conta criada em binario e escreve no "contas.txt"
+  # Função de cadastrar uma nova conta poupança, além disso transforma a conta criada em binario e escreve no "contas.txt"
   def cadastrar(usuario) do
-    #Tratamento de erro de emails repetidos em contas
+    # Tratamento de erro de emails repetidos em contas
     case busca_por_email(usuario.email) do
       nil ->
-        binary = [%__MODULE__{usuario: usuario}] ++ busca_contas()
-        |> :erlang.term_to_binary()
+        binary =
+          ([%__MODULE__{usuario: usuario}] ++ busca_contas())
+          |> :erlang.term_to_binary()
+
         File.write(@contas, binary)
-      _ -> {:error, "Conta ja cadastrada!"}
+
+      _ ->
+        {:error, "Conta ja cadastrada!"}
     end
   end
 
-  #Função que busca todas as conas que estão no arquivo contas.txt, e as transforma de binario para termo
+  # Função que busca todas as conas que estão no arquivo contas.txt, e as transforma de binario para termo
   def busca_contas do
     {:ok, binary} = File.read(@contas)
     :erlang.binary_to_term(binary)
   end
 
-  #Função para comparar um email pesquisado com os emails ja criados
+  # Função para comparar um email pesquisado com os emails ja criados
   def busca_por_email(email), do: Enum.find(busca_contas(), &(&1.usuario.email == email))
 
-  #A função a seguir é uma função de investimento
-  #de = email de uma conta_qualquer (em teoria, mas estamos usando um email de uma conta poupança)
-  #para = email de uma conta_poupança
+  # A função a seguir é uma função de investimento
+  # de = email de uma conta_qualquer (em teoria, mas estamos usando um email de uma conta poupança)
+  # para = email de uma conta_poupança
   def investir(de, para, valor_investido) do
     de = busca_por_email(de)
     para = busca_por_email(para)
+
     cond do
-      #Tratamento de erro quando Saldo < valor_investido
-      valida_saldo(de.saldo, valor_investido) -> {:error, "Saldo insuficiente!"}
+      # Tratamento de erro quando Saldo < valor_investido
+      valida_saldo(de.saldo, valor_investido) ->
+        {:error, "Saldo insuficiente!"}
+
       true ->
-        #Aqui nós deletamos as contas da lista, modificamos os valores, e depois adicionamos de volta na lista
+        # Aqui nós deletamos as contas da lista, modificamos os valores, e depois adicionamos de volta na lista
         contas = Poupanca.deletar([de, para])
         de = %Poupanca{de | saldo: de.saldo - valor_investido}
         para = %Poupanca{para | saldo: para.saldo + valor_investido}
-        contas = contas ++ [de,para]
-        #Salvando a transferência em "transacoes.txt" através do modulo de Transacao
-        Transacao.gravar("Investimento", de.usuario.email, valor_investido, Date.utc_today(), para.usuario.email)
+        contas = contas ++ [de, para]
+        # Salvando a transferência em "transacoes.txt" através do modulo de Transacao
+        Transacao.gravar(
+          "Investimento",
+          de.usuario.email,
+          valor_investido,
+          Date.utc_today(),
+          para.usuario.email
+        )
+
         File.write(@contas, :erlang.term_to_binary(contas))
     end
+
+    "Investimento realizado!"
   end
 
-  #Função de deletar conta, ou contas da lista
+  # Função de deletar conta, ou contas da lista
   def deletar(contas_deletar) do
     Enum.reduce(contas_deletar, busca_contas(), fn c, acc -> List.delete(acc, c) end)
   end
 
-  #Função de retirar/sacar valores da conta_poupança
+  # Função de retirar/sacar valores da conta_poupança
   def retirar(conta_poupanca, valor_retirado) do
     conta_poupanca = busca_por_email(conta_poupanca)
+
     cond do
-      #Tratamento de erro de quando Saldo < valor_retirado
-      valida_saldo(conta_poupanca.saldo, valor_retirado) -> {:error, "Saldo insuficiente!"}
+      # Tratamento de erro de quando Saldo < valor_retirado
+      valida_saldo(conta_poupanca.saldo, valor_retirado) ->
+        {:error, "Saldo insuficiente!"}
+
       true ->
-        #Aqui nós deletamos a contas da lista, modificamos o valor_retirado, e depois adicionamos de volta na lista
+        # Aqui nós deletamos a contas da lista, modificamos o valor_retirado, e depois adicionamos de volta na lista
         contas = Poupanca.deletar([conta_poupanca])
         conta_poupanca = %Poupanca{conta_poupanca | saldo: conta_poupanca.saldo - valor_retirado}
         contas = contas ++ [conta_poupanca]
-        #Salvando o recebimento em "transacoes.txt" através do modulo de Transacao
-        Transacao.gravar("Recebimento/Saque", conta_poupanca.usuario.email, valor_retirado, Date.utc_today())
+        # Salvando o recebimento em "transacoes.txt" através do modulo de Transacao
+        Transacao.gravar(
+          "Recebimento/Saque",
+          conta_poupanca.usuario.email,
+          valor_retirado,
+          Date.utc_today()
+        )
+
         File.write(@contas, :erlang.term_to_binary(contas))
-        {:ok, conta_poupanca, "mensagem de email encaminhada!"}
+        {:ok, conta_poupanca, "Mensagem de email encaminhada!"}
     end
   end
 
-  #Desenvolvendo a função de lucro das contas_poupanças
-  #def lucros(conta_poupança, saldo) do
-  #  conta_poupanca = busca_por_email(conta_poupanca)
+  # Desenvolvendo a função de lucro das contas_poupanças
+  def lucros(data) do
+    # Se a data atual for igual a uma data especifica faça
+    if data == ~D[2022-02-11] do
+      contas = Poupanca.deletar([busca_contas()])
+      # Atualizando todos os saldos de todas as contas usando o Enum.map
+      contas =
+        Enum.map(contas, fn x ->
+          %Poupanca{x | saldo: Float.round(x.saldo + x.saldo * 0.0076, 2)}
+        end)
 
-  #end
+      # Salvando o recebimento em "transacoes.txt" através do modulo de Transacao
+      Transacao.gravar(
+        "Lucros Concedidos",
+        "Banco",
+        "Indefinido",
+        Date.utc_today(),
+        "Contas_Poupancas"
+      )
 
-  #Função que verifica se o saldo < valor
+      File.write(@contas, :erlang.term_to_binary(contas))
+      "Lucros adicionados nas contas com sucesso!"
+      # Caso contrário
+    else
+      "Nao esta no dia para lucros!"
+    end
+  end
+
+  # Função que verifica se o saldo < valor
   defp valida_saldo(saldo, valor), do: saldo < valor
 end
--- lib/transacao.ex
+++ lib/transacao.ex
@@ -1,26 +1,38 @@
 defmodule Transacao do
-  #Definindo uma estrutura
-  defstruct data: Date.utc_today, tipo: nil, valor: 0, de: nil, para: nil
+  # Definindo uma estrutura
+  defstruct data: Date.utc_today(), tipo: nil, valor: 0, de: nil, para: nil
 
-  #Denominado o arquivo "transacoes.txt" na variavel @transacoes
+  # Denominado o arquivo "transacoes.txt" na variavel @transacoes
   @transacoes "transacoes.txt"
 
-  #Função de gravar os dados de transações ocorridas com contas poupanças
+  # Função de gravar os dados de transações ocorridas com contas poupanças
   def gravar(tipo, de, valor, data, para \\ nil) do
-    transacoes = busca_transacoes() ++
-    [%__MODULE__{tipo: tipo, de: de, valor: valor, data: data, para: para}]
-    #Escrevendo os dados no arquivo "transacoes.txt"
+      transacoes = busca_transacoes() ++
+        [%__MODULE__{tipo: tipo, de: de, valor: valor, data: data, para: para}]
+
+    # Escrevendo os dados no arquivo "transacoes.txt"
     File.write(@transacoes, :erlang.term_to_binary(transacoes))
   end
 
-  #Função de buscar todas as transações
+  # Função de buscar todas as transações
   def busca_todas(), do: busca_transacoes()
 
-  #Função de ler todas as transações no arquivo "transacoes.txt" e transformar de binario para termo
+  # Filtro de busca de transações por ano
+  def busca_por_ano(ano), do: Enum.filter(busca_transacoes(), &(&1.data.year == ano))
+
+  # Filtro de busca de transações por ano e mês
+  def busca_por_mes(ano, mes),
+    do: Enum.filter(busca_transacoes(), &(&1.data.year == ano && &1.data.month == mes))
+
+  # Filtro de busca de transações por data completa
+  def busca_por_dia(data), do: Enum.filter(busca_transacoes(), &(&1.data == data))
+
+  # Função de ler todas as transações no arquivo "transacoes.txt" e transformar de binario para termo
   defp busca_transacoes() do
-      File.read(@transacoes)
-      {:ok, binario} = File.read(@transacoes)
-      binario
-      |> :erlang.binary_to_term()
+    File.read(@transacoes)
+    {:ok, binario} = File.read(@transacoes)
+
+    binario
+    |> :erlang.binary_to_term()
   end
 end
